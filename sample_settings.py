20830e9fb2785eda94bf9e7c0dab70d476bc82b4
avi <hi@avi.im>
2015-08-31 20:52:22 +0530

    Add `sample_settings.py`

---
+++ sample_settings.py
@@ -0,0 +1,20 @@
+# Rename this file to `settings.py` in deployment
+
+# supported_subreddits = 'india'
+supported_subreddits = 'india+indianbooks'
+user_agent = ('Goodreads, v0.1. Gives info of the book whenever goodreads'
+              'link to a book is posted. (by /u/avinassh)')
+scopes = ['identity', 'submit', 'privatemessages', 'read']
+be_gentle_to_reddit = True
+
+# reddit app
+app_key = 'K...q'
+app_secret = 'y...i'
+
+# bot account
+access_token = '3...R'
+refresh_token = '3...m'
+
+# good reads
+goodreads_api_key = '5...v'
+goodreads_api_secret = 'T...4'
