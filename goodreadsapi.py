da5c132b7858bc5ca6ae6a1c1aad91031c944988
avi <hi@avi.im>
2015-12-29 19:32:41 +0530

    Update goodreadsapi for better exception handling

--- goodreadsapi.py
+++ goodreadsapi.py
@@ -7,8 +7,6 @@ import xmltodict
 
 from settings import goodreads_api_key
 
-api_url = 'http://goodreads.com/book/show/{0}?format=xml&key={1}'
-
 
 def get_goodreads_ids(comment_msg):
     # receives goodreads url
@@ -18,10 +16,15 @@ def get_goodreads_ids(comment_msg):
 
 
 def get_book_details_by_id(goodreads_id):
+    api_url = 'http://goodreads.com/book/show/{0}?format=xml&key={1}'
     r = requests.get(api_url.format(goodreads_id, goodreads_api_key))
-    book_data = xmltodict.parse(r.content)['GoodreadsResponse']['book']
-    keys = ['title', 'average_rating', 'ratings_count', 'description']
-    book = dict()
+    try:
+        book_data = xmltodict.parse(r.content)['GoodreadsResponse']['book']
+    except (TypeError, KeyError):
+        return False
+    keys = ['title', 'average_rating', 'ratings_count', 'description',
+            'num_pages']
+    book = {}
     for k in keys:
         book[k] = book_data[k]
     if type(book_data['authors']['author']) == list:
