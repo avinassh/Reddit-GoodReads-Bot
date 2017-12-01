c1bbdfe87e65bdde4b43750bf921aeab2fb496fe
avi <hi@avi.im>
2017-12-01 21:13:31 +0530

    Add common settings file

---
+++ common_settings.py
@@ -0,0 +1,7 @@
+# Common, non secret settings
+# Used in both for Docker and non-Docker deployments
+# Check `settings_sample.py` or `docker_settings.py`
+
+supported_subreddits = 'india+indianbooks+52in52+indianreaders'
+user_agent = ('Goodreads, v0.1. Gives info of the book whenever goodreads'
+              'link to a book is posted. (by /u/avinassh)')
